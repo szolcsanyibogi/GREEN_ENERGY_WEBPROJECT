859c139e82fbba6bd2b9980219bd724029eeeb50
szolcsanyi.bogi <szolcsanyi.bogi@stud.uni-obuda.hu>
2025-04-22 22:11:14 +0200

    pages mappa létrehozása

--- GREEN_ENERGY_WEBPROJECT.Endpoint/Controllers/HomeController.cs
+++ GREEN_ENERGY_WEBPROJECT.Endpoint/Controllers/HomeController.cs
@@ -23,10 +23,23 @@ namespace GREEN_ENERGY_WEBPROJECT.Endpoint.Controllers
             return View();
         }
 
+        public IActionResult Diagrams()
+        {
+            return View();
+        }
+
+        public IActionResult Upload_data()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
+    
 }
