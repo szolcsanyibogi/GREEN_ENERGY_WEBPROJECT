9abcf1fdd057484d3b542251b76c415bcb29e4ba
szolcsanyi.bogi <szolcsanyi.bogi@stud.uni-obuda.hu>
2025-05-12 22:05:46 +0200

    Done !!

--- GREEN_ENERGY_WEBPROJECT.Endpont/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GREEN_ENERGY_WEBPROJECT.Endpont.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
